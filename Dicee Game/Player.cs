c53d2b2e6bb7cb12784f19e55d0cbb67a22f178b
Karan <pate1228@sheridancollege.com>
2020-03-30 22:20:03 -0400

    Finished Player Class using the data given from the UML diagram

--- Dicee Game/Player.cs
+++ Dicee Game/Player.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Karan Patel Player Class
+
 namespace Dicee_Game
 {
+    enum RollResult  //Enum which contains the resulets
+    {
+        Win,
+        Lose,
+        Jackpot
+    }
     class Player
     {
+       
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public List<RollResult> History { get;} //stores the roll
+
+        public Player()
+        {
+            History = new List<RollResult>(); //creates/inits the list 
+        }
     }
 }

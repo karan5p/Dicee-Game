92850d1113031e2ec126175dd703691d512369d1
Karan <pate1228@sheridancollege.com>
2020-03-30 22:27:43 -0400

    Dice.cs is finished based on the UML

--- Dicee Game/Dice.cs
+++ Dicee Game/Dice.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Karan Patel Dice class 
+// This class rolls the dice and determines the amount of faces
 namespace Dicee_Game
 {
     class Dice
     {
+        public int Face { get; private set; } 
+        public int Max { get; set; }
+
+        private readonly Random random; //creating random as readonly to store the random prop value
+
+        public Dice(int max) //initilizing
+        {
+            Max = max; 
+            random = new Random();
+        }
+
+        public void Roll()
+        {
+            Face = random.Next(1, Max); // New random # is created and assigned to face
+        }
     }
 }
--- Dicee Game/DiceGame.cs
+++ Dicee Game/DiceGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dicee_Game
 {
-    class Game
+    class DiceGame
     {
     }
 }
